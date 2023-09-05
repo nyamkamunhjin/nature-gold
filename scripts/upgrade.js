324ee0fa215019a77eaf8151ed5b8f78b8c1fdd8
Munkhjin Nyamdorj <nyamkamunhjin@gmail.com>
2023-09-06 00:51:28 +0800

    Add forceImport

--- scripts/upgrade.js
+++ scripts/upgrade.js
@@ -1,14 +1,14 @@
 // scripts/upgrade_box.js
 const { ethers, upgrades } = require("hardhat");
 
-const PROXY = "";
+const PROXY = "0x865CD704052f7aBE5e1F479B98B87D9C985d8528";
 
 async function main() {
 
 
     const NatureGoldV2 = await ethers.getContractFactory("NatureGoldV2");
     console.log("Upgrading NatureGold...");
-    await upgrades.upgradeProxy(PROXY, NatureGoldV2);
+    await upgrades.forceImport(PROXY, NatureGoldV2, { kind: 'transparent' });
     console.log("NatureGold upgraded");
 }
 
